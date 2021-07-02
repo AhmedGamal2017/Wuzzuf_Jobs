3817d76ade0e24c9550645539bb6b50e17e09584
ahmedgamal218 <ahmedgamal.ag218@outlook.com>
2021-07-03 00:09:34 +0200

    Initial GUI

--- jProject/src/main/java/com/javaProject/jProject/Router.java
+++ jProject/src/main/java/com/javaProject/jProject/Router.java
@@ -1,25 +1,46 @@
 package com.javaProject.jProject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Router {
+	
+	@Value("${welcome.message:message}")
+	private String message = "Hello World";
+	
+	private List<WuzzufJob> jobs = new ArrayList<WuzzufJob>();
 
-	@RequestMapping("/gamal")
-	public String gamal(
-			/* "send  parameter" @RequestParam(value = "test", defaultValue = "non") String val */ ) 
-	{return "Gamal Page";}
-	
-	@RequestMapping("/mady")
-	public String mady(
-			/* "send  parameter" @RequestParam(value = "test", defaultValue = "non") String val */ ) 
-	{return "Mady Page";}
-	
-	@RequestMapping("/atef")
-	public String atef(
-			/* "send  parameter" @RequestParam(value = "test", defaultValue = "non") String val */ ) 
-	{return "Atef Page";}
-	
+//	@RequestMapping("/gamal")
+//	public String gamal(
+//			/* "send  parameter" @RequestParam(value = "test", defaultValue = "non") String val */ ) 
+//	{return "Gamal Page";}
+//	
+//	@RequestMapping("/mady")
+//	public String mady(
+//			/* "send  parameter" @RequestParam(value = "test", defaultValue = "non") String val */ ) 
+//	{return "Mady Page";}
+//	
+//	@RequestMapping("/atef")
+//	public String atef(
+//			/* "send  parameter" @RequestParam(value = "test", defaultValue = "non") String val */ ) 
+//	{return "Atef Page";}
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "welcome";
+	}
+
+	@RequestMapping("/next")
+	public String next(Map<String, Object> model) {
+		model.put("message", "You are in new page !!");
+		return "next";
+	}
+
 }
---
+++ jProject/src/main/webapp/WEB-INF/jsp/next.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<html lang="en">
+<body>
+    <div>
+        <div>
+            <h1>Another page</h1>
+            <h2>Hello ${message}</h2>
+             
+            Click on this <strong><a href="/">link</a></strong> to visit previous page.
+        </div>
+    </div>
+</body>
+</html>
